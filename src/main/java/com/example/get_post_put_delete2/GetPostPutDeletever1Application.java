d2c102291b8cd92e4dc8a432e34ea4996e0929ab
0227benson <benson3712297@gmail.com>
2024-04-04 22:58:36 +0800

    Rename file

--- src/main/java/com/example/get_post_put_delete2/GetPostPutDeletever1Application.java
+++ src/main/java/com/example/get_post_put_delete2/GetPostPutDeletever1Application.java
@@ -6,10 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @SpringBootApplication
-public class GetPostPutDelete2Application {
+public class GetPostPutDeletever1Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(GetPostPutDelete2Application.class, args);
+        SpringApplication.run(GetPostPutDeletever1Application.class, args);
     }
 
 
